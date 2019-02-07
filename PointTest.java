c990ea986eb54a2f18530113c299e15aafa5a3ac
Noah Pierce <noah.j.pierce@ou.edu>
2019-02-07 15:47:28 -0600

    finished creating test cases, as well as fixed the constant error

--- PointTest.java
+++ PointTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 
 public class PointTest {
 
+	private static final int CONSTANT_X = 7;
+
 	@Test
 	public void testPoint() {
 		Point p = new Point();
@@ -14,16 +16,16 @@ public class PointTest {
 
 	@Test
 	public void testPointPoint() {
-		Point p = new Point();
-		int expected = 0;
+		Point p = new Point(new Point(CONSTANT_X,CONSTANT_X));
+		int expected = CONSTANT_X;
 		int actual = p.getX();
 		assertEquals(expected, actual);
 	}
 
 	@Test
 	public void testPointIntInt() {
-		Point p = new Point();
-		int expected = 0;
+		Point p = new Point(CONSTANT_X,CONSTANT_X);
+		int expected = CONSTANT_X;
 		int actual = p.getX();
 		assertEquals(expected, actual);
 	}
